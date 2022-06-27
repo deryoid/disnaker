f50351c36effe506bcd545cd3db38f091e335800
deryoid <dery037yj@gmail.com>
2022-06-27 19:10:46 +0800

    pk

--- db/disnaker.sql
+++ db/disnaker.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Waktu pembuatan: 04 Des 2021 pada 08.51
--- Versi server: 5.7.24
--- Versi PHP: 7.4.12
+-- Host: localhost:8889
+-- Waktu pembuatan: 27 Jun 2022 pada 11.08
+-- Versi server: 5.7.34
+-- Versi PHP: 7.4.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,8 +18,21 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `pkl_disnaker`
+-- Database: `disnaker`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `instansi`
+--
+
+CREATE TABLE `instansi` (
+  `id_instansi` int(11) NOT NULL,
+  `nama_instansi` varchar(255) NOT NULL,
+  `info_instansi` varchar(255) NOT NULL,
+  `alamat_instansi` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -39,13 +52,6 @@ CREATE TABLE `masyarakat` (
   `no_wa` varchar(25) DEFAULT NULL,
   `status` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `masyarakat`
---
-
-INSERT INTO `masyarakat` (`id_masyarakat`, `id_user`, `nama_masyarakat`, `nik`, `email`, `jk`, `alamat`, `pekerjaan`, `no_wa`, `status`) VALUES
-(3, 12, 'Ella', '637239990011', 'ella@gmail.com', 'Perempuan', 'Kandangan', 'Belum Ada', '-', 'Aktif');
 
 -- --------------------------------------------------------
 
@@ -67,13 +73,6 @@ CREATE TABLE `pendaftaran` (
   `id_masyarakat` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data untuk tabel `pendaftaran`
---
-
-INSERT INTO `pendaftaran` (`id_pendaftaran`, `nomor_antrian`, `ktp`, `kk`, `foto`, `tgl_pendaftaran`, `tgl_buat`, `tgl_ambil`, `ket`, `status_pendaftaran`, `id_masyarakat`) VALUES
-(12, '001', '15053.jpeg', '87933.png', '19603.JPG', '2021-12-04', '2021-12-05', '2021-12-05', '1', 'Selesai', 3);
-
 -- --------------------------------------------------------
 
 --
@@ -92,12 +91,17 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id_user`, `username`, `password`, `role`) VALUES
-(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Administrator'),
-(12, 'Ella', '202cb962ac59075b964b07152d234b70', 'Masyarakat');
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Administrator');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `instansi`
+--
+ALTER TABLE `instansi`
+  ADD PRIMARY KEY (`id_instansi`);
 
 --
 -- Indeks untuk tabel `masyarakat`
@@ -124,6 +128,12 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `instansi`
+--
+ALTER TABLE `instansi`
+  MODIFY `id_instansi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT untuk tabel `masyarakat`
 --
 ALTER TABLE `masyarakat`
@@ -140,6 +150,22 @@ ALTER TABLE `pendaftaran`
 --
 ALTER TABLE `user`
   MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `masyarakat`
+--
+ALTER TABLE `masyarakat`
+  ADD CONSTRAINT `masyarakat_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+
+--
+-- Ketidakleluasaan untuk tabel `pendaftaran`
+--
+ALTER TABLE `pendaftaran`
+  ADD CONSTRAINT `pendaftaran_ibfk_1` FOREIGN KEY (`id_masyarakat`) REFERENCES `masyarakat` (`id_user`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
